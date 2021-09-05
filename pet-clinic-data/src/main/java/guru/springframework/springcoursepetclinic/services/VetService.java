5fcac2453af66c750d5da7a2ca29457ae6937880
virajbukitagar123 <viraj.bukitagar@gmail.com>
2021-09-05 15:40:56 +0530

    Implement Interfaces for Services

---
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package guru.springframework.springcoursepetclinic.services;
+
+import guru.springframework.springcoursepetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}

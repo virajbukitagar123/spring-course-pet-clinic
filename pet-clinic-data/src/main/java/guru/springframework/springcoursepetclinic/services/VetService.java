a46f69560a443cc18b7bd8da63198451fc74d0b0
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-14 20:38:23 +0530

    Adding service interfaces. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package guru.springframework.springcoursepetclinic.services;
+
+import guru.springframework.springcoursepetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

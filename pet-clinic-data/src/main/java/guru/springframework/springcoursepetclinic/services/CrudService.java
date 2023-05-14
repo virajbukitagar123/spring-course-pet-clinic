0f9a1a69f35897de9ca0908dac4b3278245ef70c
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-14 21:40:42 +0530

    refactoring to crud interface. Closes #11.

---
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/services/CrudService.java
@@ -0,0 +1,11 @@
+package guru.springframework.springcoursepetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+    T findById(ID id);
+    T save(T Object);
+    void delete(T object);
+    void deleteById(ID id);
+}

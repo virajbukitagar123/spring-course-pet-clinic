2ebb015c9cf8bf31b7d3baa27c5039a17c6d79ae
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-14 15:20:43 +0530

    Initial Project Add.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-course-pet-clinic
+
 Pet Clinic application which is built from ground up as part of Spring Framework 5: Beginner to Guru Course
---
+++ src/test/java/guru/springframework/springcoursepetclinic/SpringCoursePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springcoursepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoursePetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

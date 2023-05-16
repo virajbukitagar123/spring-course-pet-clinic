f12b4f7572f810a924be075a5afb626ed144ec21
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-17 01:21:52 +0530

    Added SpecialityServiceMap. Closes #33.

---
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springcoursepetclinic.services;
+
+import guru.springframework.springcoursepetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}

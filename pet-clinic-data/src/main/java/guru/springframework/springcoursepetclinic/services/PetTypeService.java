68da1921a492aad806355fa469166c052bb49095
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-17 00:25:43 +0530

    Implemented the PetType Map Service. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springcoursepetclinic.services;
+
+import guru.springframework.springcoursepetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}

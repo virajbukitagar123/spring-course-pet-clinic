8239aafb790ecf91e5b52d132d2714506eea692b
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-18 01:25:22 +0530

    Created a Visit Map Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springcoursepetclinic.services;
+
+import guru.springframework.springcoursepetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}

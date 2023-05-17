911866c93d26f053560ce95fb8dab6e132281660
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-18 00:21:52 +0530

    Implemented Vet and Speciality Entity using JPA. Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package guru.springframework.springcoursepetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialies",
+    joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {

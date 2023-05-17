2d7b52c5f6dde6c7c6367c537e5578208ec491c4
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-17 23:48:21 +0530

    Implemented Base Entity using JPA. Closes #35.

--- pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/BaseEntity.java
@@ -1,8 +1,14 @@
 package guru.springframework.springcoursepetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {

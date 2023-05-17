c6e8a7213462836c1273f86fbf855de6c136fa14
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-18 00:08:16 +0530

    Implemented Owner Entity using JPA. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/Person.java
@@ -1,8 +1,14 @@
 package guru.springframework.springcoursepetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/springcoursepetclinic/model/PetType.java
@@ -1,5 +1,10 @@
 package guru.springframework.springcoursepetclinic.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
     private String name;
 

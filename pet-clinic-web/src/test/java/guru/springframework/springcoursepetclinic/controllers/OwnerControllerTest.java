b9966d3a0ef98c7d7a3b8e145883765158f21db8
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-06-03 23:59:55 +0530

    Adding test for owners List flow. Closes #67.

--- pet-clinic-web/src/test/java/guru/springframework/springcoursepetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/springcoursepetclinic/controllers/OwnerControllerTest.java
@@ -141,4 +141,17 @@ class OwnerControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/owners/1"));
     }
+
+
+    @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
+                        Owner.builder().id(2L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
 }
